f7ff5c025e65f658d441c5d30324c6c62a06836d
osch7497 <osch7497@gmail.com>
2026-01-05 17:11:42 +0900

    LaycastUI Edit
    LaycastUI Edit

--- Assets/1_Scripts/SetUITextScript.cs
+++ Assets/1_Scripts/SetUITextScript.cs
@@ -1,14 +1,21 @@
-using System;
 using TMPro;
 using UnityEngine;
 
 public class SetUITextScript : MonoBehaviour
 {
     TextMeshProUGUI GuideText;
+    GameObject Player;
     void Start()
     {   
+        Player = GameObject.FindWithTag("MainCamera");
+        transform.localPosition = Vector3.zero;
         GuideText = GetComponentInChildren<TextMeshProUGUI>();
         GuideText.text = $"{transform.parent.name}를 줍기";
         Debug.Log($"변경된 내용:{GuideText.text}");
+    }
+    void Update() {
+        Transform target = GameObject.FindWithTag("MainCamera").transform; 
+        transform.rotation = Player.transform.rotation;    
+        transform.Rotate(0f,180f,0f);
     }
 }
