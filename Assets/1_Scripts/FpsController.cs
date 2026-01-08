a553aad68ae282d2b256fbb0dafd09c4d74226dc
FACT0R1AL <factorial4861@gmail.com>
2026-01-08 14:20:04 +0900

    STAIR COMPLETED

--- Assets/1_Scripts/FpsController.cs
+++ Assets/1_Scripts/FpsController.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class FpsController : MonoBehaviour
 {
     public float moveSpeed = 3f;
     public float multiplierRunSpeed = 1.5f;
     public float lookSpeed = 15f;
+    public float maxRunningTime = 10f;
+    public float maxFloatingTime = 13f;
+    public Image staminaBar;
 
     public Transform playerCamera;
     private Vector3 cameraOriginPos;
@@ -13,6 +17,7 @@ public class FpsController : MonoBehaviour
     private Vector2 moveInput;
     private Vector2 lookInput;
     private bool isRunning = false;
+    private float currentStamina;
     private float xRotation;
     private float sinWaving;
     
@@ -34,6 +39,7 @@ public class FpsController : MonoBehaviour
         Cursor.visible = false;
         
         cameraOriginPos = playerCamera.localPosition;
+        currentStamina = 100f;
     }
     
     void OnEnable() => playerInput.Enable();
@@ -52,12 +58,17 @@ public class FpsController : MonoBehaviour
         velocity.x = moveDir.x * moveSpeed;
         velocity.z = moveDir.z * moveSpeed;
 
-        if (isRunning)
+        if (isRunning && currentStamina > 0.1f)
         {
             velocity.x *= multiplierRunSpeed;
             velocity.z *= multiplierRunSpeed;
+            currentStamina -= (100f / maxRunningTime) * Time.deltaTime;
         }
-
+        
+        if (currentStamina < 100f && !isRunning) currentStamina += (100f / maxFloatingTime) * Time.deltaTime;
+        
+        Debug.Log(currentStamina);
+        Debug.Log(velocity);
         rb.linearVelocity = velocity;
         
         // 화면 이동
@@ -72,10 +83,12 @@ public class FpsController : MonoBehaviour
             playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         }
         
+        
+        // 머리 흔들림
         if (moveInput != Vector2.zero)
         {
-            sinWaving += Time.deltaTime * (isRunning ? 15f : 10f);
-            float yOffset = Mathf.Sin(sinWaving) * (isRunning ? 0.14f : 0.07f);
+            sinWaving += Time.deltaTime * ((isRunning && currentStamina > 0.1f) ? 14f : 9f);
+            float yOffset = Mathf.Sin(sinWaving) * ((isRunning && currentStamina > 0.1f) ? 0.13f : 0.07f);
 
             playerCamera.transform.localPosition =
                 cameraOriginPos + new Vector3(0f, yOffset, 0f);
@@ -84,8 +97,7 @@ public class FpsController : MonoBehaviour
         {
             sinWaving = 0f;
             playerCamera.transform.localPosition =
-                Vector3.Lerp(playerCamera.transform.localPosition, cameraOriginPos, Time.deltaTime * 10f);
+                Vector3.Lerp(playerCamera.transform.localPosition, cameraOriginPos, Time.deltaTime * 20f);
         }
-
     }
 }
