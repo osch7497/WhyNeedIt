c352fa68ea52aabfaa8b410ca8d22577a2fd820b
osch7497 <osch7497@gmail.com>
2026-01-08 11:36:24 +0900

    rayder
    rayder

--- Assets/1_Scripts/MonsterMovementScript.cs
+++ Assets/1_Scripts/MonsterMovementScript.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
@@ -7,9 +8,22 @@ public class MonsterMovementScript : MonoBehaviour
 {
     NavMeshAgent Agent;
     Animator Anim;
-    public Transform target;
-    public Transform WayPointGroup;
+    private Transform target;
     private Transform[] WayPoints;
+    [Header("TargetGroup")]
+    [SerializeField] private Transform WayPointGroup;
+    [Header("RaySetting")]
+    [SerializeField] float SightAngle; 
+    [SerializeField] float SightRange; 
+    
+    
+    [Header("MaskSetting")]
+    [SerializeField] private LayerMask targetMask;          // 탐색 대상
+    [SerializeField] private LayerMask obstacleMask;        // 장애물 대상
+
+    [Header("Draw Line")]
+    [Range(0.01f, 1f)]
+    [SerializeField] private float angle; //선 1개 1개의 각도.
     void Awake()
     {
         Anim = GetComponent<Animator>();
@@ -22,8 +36,20 @@ public class MonsterMovementScript : MonoBehaviour
         StartCoroutine(co);
     }
     void SetRandomPoint(){
-        target = WayPoints[Random.Range(0,WayPoints.Length)];
+        target = WayPoints[UnityEngine.Random.Range(0,WayPoints.Length)];
         Debug.Log($"Waypoint Setted : {target}");
+    }
+    IEnumerator GetSight(){
+        float FAA = SightAngle/2f;//final add angle
+        float NSA = -SightAngle/2f;//now sight angle
+        while(NSA < FAA){
+            NSA+=angle;
+            RaycastHit Hitinfo;
+            Physics.Raycast(transform.position + new Vector3(0,1f,0),transform.rotation * Quaternion.Euler(0,NSA,0) * Vector3.forward,out Hitinfo,15f,obstacleMask);
+            float Distance = (Hitinfo.collider != null) ? Vector3.Distance(transform.position,Hitinfo.collider.transform.position) : 15f;
+            Debug.DrawRay(transform.position + new Vector3(0,1f,0),transform.rotation * Quaternion.Euler(0,NSA,0) * Vector3.forward * Distance,Color.green);
+        }
+        yield return null;
     }
     IEnumerator MoneterAI(){
         if(target == null)
@@ -47,7 +73,7 @@ public class MonsterMovementScript : MonoBehaviour
                     SetRandomPoint();
                 }
             }
-            yield return new WaitForSeconds(Time.deltaTime);
+            yield return new WaitForSeconds(0.1f);
         }
         
     }
@@ -55,6 +81,10 @@ public class MonsterMovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        IEnumerator co = GetSight();
+        StartCoroutine(co);
+        StartCoroutine(GetSight());
         Debug.Log($"Agent enabled:{Agent.enabled}");
     }
 }
